5c251f5022b30fb42357d78fe6de447d2e30a043
Michael Sokol <michael.sokol@algolia.com>
2017-05-12 17:44:58 +0200

    docs(categories): Add JSDoc

--- examples/categories/index.js
+++ examples/categories/index.js
@@ -1,5 +1,8 @@
 const algoliaSitemap = require("../../");
 
+/**
+ * @type {algoliaConfig}
+ */
 const algoliaConfig = {
   appId: "APP_ID",
   apiKey: "API_KEY",
@@ -7,6 +10,23 @@ const algoliaConfig = {
 };
 
 const alreadyAdded = {};
+
+/**
+ * @typedef {Object} Params
+ * @property {string} loc the link of this hit
+ * @property {string} [lastmod] the last time this link was modified (ISO8601)
+ * @property {number} [priority] the priority you give to this link (between 0 and 1)
+ * @property {Object} [alternates] alternative versions of this link (useful for multi-language)
+ * @property {Array} [alternates.languages] list of languages that are enabled
+ * @property {function} [alternates.hitToURL] function to transform a language into a url of this object
+ */
+
+/**
+ * Function to transform a hit into its link
+ * 
+ * @param {Object} hit a hit to transform
+ * @returns {Params|Array.<Params>} the parameters for this hit
+ */
 function hitToParams({ objectID, category }) {
   if (!category || !category.length || category.some(c => alreadyAdded[c])) {
     return false;
@@ -17,6 +37,11 @@ function hitToParams({ objectID, category }) {
   }));
 }
 
+/**
+ * @param {algoliaConfig} algoliaConfig configuration for Algolia
+ * @param {Object} sitemapLocation an object with the href where your sitemaps will be and the relative path where to generate them
+ * @param {function} hitToParams function to transform a hit into Params
+ */
 algoliaSitemap({
   algoliaConfig,
   sitemapLocation: {
